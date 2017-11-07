384a085d0000097271d1c6fb748f8c5b2efa9fe8
Steve Mussmann <smussmann@gmail.com>
2017-11-07 10:34:06 -0600

    Now parses GFF file into dictionary of lists (key=genbank_accn_no.)

--- checkGFF.py
+++ checkGFF.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from comline import ComLine
+from gff import GFF
 
 import sys
 
 def main():
 	input = ComLine(sys.argv[1:])
+	gff=GFF(input.args.gff)
 
 main()
 
---
+++ gff.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+import collections
+
+class GFF():
+	'Class for working with GFF file'
+	
+
+	def __init__(self, f):
+		#member variables
+		self.gff_file = f
+		self.content = list()
+		self.features = collections.defaultdict(list)
+		
+		#functions
+		self.readFile()
+		#self.printf()
+		self.parseFile()
+
+	def readFile(self):
+		with open(self.gff_file) as f:
+			self.content = [line for line in f.readlines() if not line.startswith('#')]
+		self.content = [x.strip() for x in self.content]
+
+	def printf(self):
+		for x in self.content:
+			print(x)
+
+	def parseFile(self):
+		for line in self.content:
+			temp = line.split()
+			accn = temp[0].split('.')
+			self.features[accn[0]].append(line)
+		#print(self.features)
